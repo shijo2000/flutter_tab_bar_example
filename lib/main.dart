256cf223af1af3a1cc1f7fd4f690f23e42414afe
shijo2000 <shijo9863@gmail.com>
2023-09-13 17:55:12 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+class MyTabBarApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3, // Number of tabs
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('TabBar Example'),
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'Tab 1'),
+                Tab(text: 'Tab 2'),
+                Tab(text: 'Tab 3'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Center(child: Text('Tab 1 Content')),
+              Center(child: Text('Tab 2 Content')),
+              Center(child: Text('Tab 3 Content')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyTabBarApp());
+}
